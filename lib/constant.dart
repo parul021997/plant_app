a90d661b5c3c8daf257f9e9d1518623cb742f544
parul verma <parulverma02016@gmail.com>
2022-05-16 17:24:52 +0530

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF00796B);
+const kTextColor = Color(0XFF3C4046);
+const kBackgroundColor = Color(0XFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
+
---
+++ lib/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(icon: SvgPicture.asset("assets/icon/menu.svg"),onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
